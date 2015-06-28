72bb1824e998721b5933696fc8c035fb5c708812
elkraneo <cristian@metodowhite.com>
2015-06-28 14:19:47 +0200

    reinit

---
+++ Mowgli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mowgli
+//
+//  Created by Cristian Díaz on 28/06/15.
+//  Copyright © 2015 metodowhite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ mowgliTests/mowgliTests.swift
@@ -0,0 +1,35 @@
+//
+//  mowgliTests.swift
+//  mowgliTests
+//
+//  Created by Cristian Díaz on 28/06/15.
+//  Copyright © 2015 metodowhite. All rights reserved.
+//
+
+import XCTest
+
+class mowgliTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
