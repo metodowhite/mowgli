b34bc93f625007617d6afd5bcff57ebfe3985121
Cristian Díaz <cristian@metodowhite.com>
2016-02-06 15:20:51 +0100

    MovieList Structure

--- Mowgli/Bagheera/BGHTMDbClient.swift
+++ Mowgli/Bagheera/BGHTMDbClient.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import Freddy
 
 //MARK: - API URL
 let TMDbAPIBaseURL = "https://api.themoviedb.org/3/"
@@ -17,38 +18,70 @@ let TMDbAPIBaseURL = "https://api.themoviedb.org/3/"
 let TMDbConfiguration = "configuration"
 
 
-public enum MovieList: String {
-    case Latest = "movie/latest"
-    case Upcoming = "movie/upcoming"
-    case NowPlaying = "movie/now_playing"
-    case Popular = "movie/popular"
-    case TopRated = "movie/top_rated"}
+public enum MovieListEndpoint: String {
+	case Latest = "movie/latest"
+	case Upcoming = "movie/upcoming"
+	case NowPlaying = "movie/now_playing"
+	case Popular = "movie/popular"
+	case TopRated = "movie/top_rated"
+}
+
+//public enum MovieListError: ErrorType {
+//	case NoResponse
+//	case NoData
+//}
+
 
 public struct BGHTMDbClient {
-    
-    public let APIKey: String
-    
-    public func fetchMovieList(list: MovieList) {
-        Alamofire.request(.GET, TMDbAPIBaseURL + list.rawValue, parameters: ["api_key": APIKey])
-            .responseJSON { response in
-                print(response.request)  // original URL request
-                print(response.response) // URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
-                
-                if let JSON = response.result.value {
-                    print("JSON: \(JSON)")
-                }
-        }
-    }
-    
-    public init(APIKey key: String) {
-        self.APIKey = key
-    }
-    
+	
+	public let APIKey: String
+	
+	public func fetchMovieList(list: MovieListEndpoint) -> MovieList {
+		Alamofire.request(.GET, TMDbAPIBaseURL + list.rawValue, parameters: ["api_key": APIKey])
+			.responseJSON { response in
+				
+				guard let JSONData = response.data else {
+					return
+				}
+				
+				do {
+					let json = try JSON(data: JSONData)
+					let movieList = try MovieList(json: json)
+					
+					return movieList
+					print(movieList)
+					
+				} catch {
+					//
+				}
+		}
+	}
+	
+	public init(APIKey key: String) {
+		self.APIKey = key
+	}
+	
 }
 
 
-struct Movie {
-    //
+public struct MovieList {
+	public let results: Array<JSON>
+}
+
+extension MovieList: JSONDecodable {
+	public init(json value: JSON) throws {
+		results = try value.array("results")
+	}
+}
+
+public struct Movie {
+	public let title: String
+	public let overview: String
+}
+
+extension Movie: JSONDecodable {
+	public init(json value: JSON) throws {
+		title = try value.string("title")
+		overview = try value.string("overview")
+	}
 }
