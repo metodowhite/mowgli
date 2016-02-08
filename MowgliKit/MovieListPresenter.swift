3003b7c205e4121600bf8d3f427029b20959fe07
Cristian Díaz <cristian@metodowhite.com>
2016-02-08 22:43:26 +0100

    presenter temp comment

--- MowgliKit/MovieListPresenter.swift
+++ MowgliKit/MovieListPresenter.swift
@@ -7,13 +7,13 @@
 //
 
 import Foundation
-import Bagheera
+//import Bagheera
 
 final public class MovieListPresenter  {
 	
-	public private(set) var movieList: [Movie] = []
+//	public private(set) var movieList: [Movie] = []
 	
-//	let testBagheera = BGHTMDbClient(APIKey: "4552c3fa51f05ffc09b73912931a5406")
-//	testBagheera.fetchMovieList(.NowPlaying)
+	//	let testBagheera = BGHTMDbClient(APIKey: "4552c3fa51f05ffc09b73912931a5406")
+	//	testBagheera.fetchMovieList(.NowPlaying)
 }
 
